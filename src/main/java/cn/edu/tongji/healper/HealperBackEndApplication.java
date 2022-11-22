51024698270abbc0497b76ca719893e1df4c305b
Gxyrious <2466445001@qq.com>
2022-11-22 23:44:00 +0800

    init

---
+++ src/main/java/cn/edu/tongji/healper/HealperBackEndApplication.java
@@ -0,0 +1,16 @@
+package cn.edu.tongji.healper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lc2002
+ */
+@SpringBootApplication
+public class HealperBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealperBackEndApplication.class, args);
+    }
+
+}
