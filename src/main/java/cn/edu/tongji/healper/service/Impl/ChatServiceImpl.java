a52f25c94e3c5254c5008989ffd885d287bbb4f0
Gxyrious <2466445001@qq.com>
2022-12-04 20:37:16 +0800

    修复聊天内容不保存的问题

--- src/main/java/cn/edu/tongji/healper/service/Impl/ChatServiceImpl.java
+++ src/main/java/cn/edu/tongji/healper/service/Impl/ChatServiceImpl.java
@@ -4,7 +4,11 @@ import cn.edu.tongji.healper.entity.ChatMessageEntity;
 import cn.edu.tongji.healper.repository.ChatMessageRepository;
 import cn.edu.tongji.healper.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChatServiceImpl implements ChatService {
@@ -19,11 +23,22 @@ public class ChatServiceImpl implements ChatService {
         chatMessage.setClientId(clientId);
         chatMessage.setConsultantId(consultantId);
         chatMessage.setCreateTime(System.currentTimeMillis());
+        chatMessage.setContent(content);
         try {
             return chatMessageRepository.save(chatMessage);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    @Override
+    public List<ChatMessageEntity> findChatMessageEntitiesByClientIdAndConsultantId(
+            Integer clientId, Integer consultantId, Integer page, Integer size
+    ) {
+        Pageable pageRequest = PageRequest.of(page - 1, size);
+        List<ChatMessageEntity> messages =  chatMessageRepository
+                .findChatMessageEntitiesByClientIdAndConsultantId(clientId, consultantId, pageRequest);
+        return messages;
     }
 }
