e7deb9ba97768503a6fd960a934f26a762180daf
Gxyrious <2466445001@qq.com>
2022-12-07 22:36:37 +0800

    移动Websocket

--- src/main/java/cn/edu/tongji/healper/controller/WebSocketServer.java
+++ src/main/java/cn/edu/tongji/healper/controller/WebSocketServer.java
@@ -1,4 +1,4 @@
-package cn.edu.tongji.healper.component;
+package cn.edu.tongji.healper.controller;
 
 import cn.edu.tongji.healper.entity.ChatMessageEntity;
 import cn.edu.tongji.healper.entity.ClientEntity;
